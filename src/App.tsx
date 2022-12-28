32a154c5f1ccee2df5e0bdb4195de1058fb32de1
Allan Da Silva Pessin <allan.pessin@outlook.com>
2022-12-27 23:29:39 -0300

    módulos css adicinoado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.app}>
       <h1>Github Finder</h1>
       <Outlet />
     </div>
